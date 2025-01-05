cd1ac52e1ea8a1be3333d7f4fc87252145cdd282
rheannemcintosh <rheannemcintosh@gmail.com>
2025-01-05 00:04:16 +0000

    Update the create and edit files for the categories forms
    [AT-18]

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,7 +1,20 @@
- <h1>Edit Category</h1>
-    <form action="{{ route('categories.update', $category->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <input type="text" name="title" value="{{ $category->title }}">
-        <button type="submit">Update</button>
-    </form>
+<h1>Edit Category</h1>
+<form action="{{ route('categories.update', $category->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <div>
+        <div>
+            <label for="title">Category Title</label>
+            <input type="text" name="title" placeholder="Category Title" value="{{ $category->title }}">
+        </div>
+        <div>
+            <label for="description">Category Description</label>
+            <textarea name="description" placeholder="Category Description" value="{{ $category->description }}"></textarea>
+        </div>
+        <div>
+            <label for="is_active">Is Active?</label>
+            <input type="checkbox" name="is_active" id="is_active" value="true" @if ($category->is_active) checked @endif>
+        </div>
+    </div>
+    <button type="submit">Update</button>
+</form>
