2f82c8f8c872d57270b2b3fbd85ad231882ee8d8
rheannemcintosh <rheannemcintosh@gmail.com>
2025-01-05 00:04:55 +0000

    Add show to the categories index
    [AT-18]

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,6 +4,7 @@
     @foreach ($categories as $category)
         <li>
             {{ $category->title }}
+            <a href="{{ route('categories.show', $category->id) }}">Show</a>
             <a href="{{ route('categories.edit', $category->id) }}">Edit</a>
             <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
                 @csrf
