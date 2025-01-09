2d65c9dd2cb929fc8489b0a5e4363db51397268d
rheannemcintosh <rheannemcintosh@gmail.com>
2025-01-09 21:33:03 +0000

    Update the tasks migration
    [AT-20]

--- database/migrations/2024_12_25_031916_create_tasks_table.php
+++ database/migrations/2024_12_25_031916_create_tasks_table.php
@@ -14,12 +14,16 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('task_detail_id')->constrained();
-            $table->foreignId('to_do_list_id')->constrained();
+            $table->date('due_date')->nullable();
+            $table->date('started_at')->nullable();
+            $table->date('completed_at')->nullable();
             $table->enum('status', [
                 'To Do',
                 'In Progress',
                 'Done',
+                'Cancelled',
             ]);
+            $table->json('to_do_list_ids')->nullable();
             $table->timestamps();
         });
     }
