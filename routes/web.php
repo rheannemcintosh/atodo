e9f057feee959bf5b1425125d48b0c85750994bb
rheannemcintosh <rheannemcintosh@gmail.com>
2025-02-01 23:24:50 +0000

    Return the new home page as the main route
    [AT-29]

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ToDoListController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
