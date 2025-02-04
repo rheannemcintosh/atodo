73b718db4007c8811087b1841a6b68032ca2bd0d
rheannemcintosh <rheannemcintosh@gmail.com>
2025-02-04 00:57:46 +0000

    Create a projects migration
    [AT-33]

---
+++ database/migrations/2025_02_02_235915_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->date('started_at')->nullable();
+            $table->date('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
