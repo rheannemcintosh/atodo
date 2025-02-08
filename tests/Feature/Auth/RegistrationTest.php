fc4263eba3eb84d482ef472e878b50cfa31b64e0
rheannemcintosh <rheannemcintosh@gmail.com>
2025-02-08 15:09:46 +0000

    Create a new test to check that the registration redirects correctly if the email is not in the list of config app override emails
    [AT-40]

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -20,3 +20,17 @@ test('new users can register if they are in the list of app override emails', fu
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
 });
+
+test('new users cannot register if they are not in the list of app override emails', function () {
+
+    config(['app.override_emails' => []]);
+
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $response->assertRedirect(route('home', absolute: false));
+});
