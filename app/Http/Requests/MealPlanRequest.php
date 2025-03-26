c49404b5f1517772051c7fad0ab45cc3fb833d0a
rheannemcintosh <rheannemcintosh@gmail.com>
2025-03-26 00:59:37 +0000

    Create the Meal Plan Request
    [AT-46]

---
+++ app/Http/Requests/MealPlanRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class MealPlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'date'            => 'required|date',
+            'breakfast'       => 'required|string',
+            'morning_snack'   => 'required|string',
+            'lunch'           => 'required|string',
+            'afternoon_snack' => 'required|string',
+            'dinner'          => 'required|string',
+            'nighttime_snack' => 'required|string',
+        ];
+    }
+}
