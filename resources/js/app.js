9a3c70f404da1ee05faf147fab4e2e1c39420dd7
rheannemcintosh <rheannemcintosh@gmail.com>
2025-04-04 00:39:58 +0100

    Update app.js to create an inertia app
    [AT-48]

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props }) {
+        createApp({ render: () => h(App, props) }).mount(el);
+    },
+});
