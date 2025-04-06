ce5671649803a17877f82da2d7e93ae2defac31a
rheannemcintosh <rheannemcintosh@gmail.com>
2025-04-07 00:41:25 +0100

    Update vite to use app.ts
    [AT-51]

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.ts'])
         @inertiaHead
     </head>
 
