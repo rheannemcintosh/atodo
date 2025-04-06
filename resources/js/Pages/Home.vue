86a1728f62e7af922776d32777817be85bafa0b6
rheannemcintosh <rheannemcintosh@gmail.com>
2025-04-06 22:26:40 +0100

    Create a home vue component
    [AT-49]

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex items-center justify-center">
+        <div class="p-2">
+            <h1 class="">4th April 2025</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+    h1 {
+        @apply py-16 text-white font-atodo text-center font-black text-7xl;
+    }
+</style>
