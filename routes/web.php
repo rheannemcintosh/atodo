0e86d5fcf7b5511e01b0ea0e3d9dc6dba2cc07b7
rheannemcintosh <rheannemcintosh@gmail.com>
2025-04-06 22:12:22 +0100

    Remove the dashboard route
    [AT-49]

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
